d4825373d831d85fdd2ac77d533139af154715d9
m1x74 <stasik747474@gmail.com>
2021-11-28 17:57:14 +0300

    add6

---
+++ lesson6/add/js.js
@@ -0,0 +1,141 @@
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+// function cutString(str, n) {
+//     let array=[];
+//     for (let i = 0; i < str.length; i+=n) {
+//         array.push(str.substr(i,n));
+//     }
+//  return array
+// }
+//
+// console.log(cutString('наслаждение',3));
+
+// - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
+// Протестувати на значеннях
+// someemail@gmail.com
+// someeMAIL@gmail.com
+// someeMAIL@i.ua
+// some.email@gmail.com
+// function looksLikeMail(str) {
+//     let ravlik = str.indexOf('@');
+//     let re = str.split('@');
+//     let abc = re[0];
+//     let lastDotPos = re[1].lastIndexOf('.');
+//     return (abc.length > 1 && ravlik !== -1 && lastDotPos > 2  && (str.length - lastDotPos) > 2);
+//
+// }
+//
+//
+// console.log(looksLikeMail('someemail@gmail.com'))
+// console.log(looksLikeMail('someeMAIL@gmail.com'))
+// console.log(looksLikeMail('someeMAIL@i.ua'))
+// console.log(looksLikeMail('some.email@gmail.com'))
+//
+// if (looksLikeMail('someeMAIL@gmail.com')===true){
+//     console.log('valid')
+// } else {
+//     console.log('ne valid')
+// }
+
+// - є масивlet
+// coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+//
+// // відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+// let f=(mod)=>{
+//     return mod.sort((a,b)=>b.modules.length-a.modules.length);
+// }
+// console.log(f(coursesArray));
+
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+// let str="Астрономия это наука о небесных объектах";
+// let symb = "о";
+// let count=(str, stringsearch)=>{
+//     return str.split(stringsearch).length-1;
+//
+// }
+// console.log(count(str,symb));
+// document.writeln(count(str, symb));
+
+// - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+// let str = "Сила тяжести приложена к центру масс тела";
+// // console.log(str.split(' '))
+// let cutString=(str, n)=>{
+//     return str.split(' ').slice(0,n).join(' ');
+//
+// }
+// console.log(cutString(str, 5));
+
+
