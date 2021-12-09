1827762b6b44a520bee81cb468b006c419eafde9
m1x74 <stasik747474@gmail.com>
2021-12-09 14:45:44 +0300

    made hw11

---
+++ les11/hw/js.js
@@ -0,0 +1,36 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+let name=document.form.name;
+let age=document.form.age;
+let btn=document.getElementById('btn');
+let func=(userName,userAge)=>{
+    let object={user:userName,age:userAge};
+    localStorage.setItem('key',JSON.stringify(object));
+};
+btn.onclick=function () {
+    func(name.value,age.value)
+}
+
+
+
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+let btn1=document.getElementById('btncar');
+let volume=document.form1.volume;
+let model=document.form1.model;
+let type=document.form1.type;
+
+let a=(carModel,carType,carVolume)=>{
+    let obj={
+        model:carModel,
+        type:carType,
+        volume:carVolume
+
+    };
+    localStorage.setItem('guest',JSON.stringify(obj));
+}
+btn1.onclick=(f)=>{
+    a(model.value,type.value,volume.value);
+}
