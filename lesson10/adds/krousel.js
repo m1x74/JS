134dc9178ea2c361996e0fc7db991e20e34e7178
m1x74 <stasik747474@gmail.com>
2021-12-10 00:33:02 +0300

    made add10

---
+++ lesson10/adds/krousel.js
@@ -0,0 +1,20 @@
+// - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
+const left=document.querySelector('.btn-left');
+const right=document.querySelector('.right');
+const slider=document.querySelector('.carousel_slide');
+const images=document.querySelectorAll('.carousel_slide img');
+let counter =0;
+const stepSize=images[0].clientWidth;
+slider.style.transform= 'translatex('+ `${-stepSize*counter}px)`;
+right.addEventListener('click',()=>{
+    if (counter>=images.length-1) counter=-1;
+    slider.classList.add('transformAnim')
+    counter++;
+    slider.style.transform= 'translatex('+ `${-stepSize*counter}px)`;
+})
+left.addEventListener('click',()=>{
+    if (counter<=0) counter=images.length;
+    slider.classList.add('transformAnim')
+    counter--;
+    slider.style.transform= 'translatex('+ `${-stepSize*counter}px)`;
+})
